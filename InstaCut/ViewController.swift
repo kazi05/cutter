f83849f620f9f696df72b17f6320df3dacc14bc5
Kazim Gajiev <mr.gadjiev2011@yandex.ru>
2018-02-15 14:46:07 +0300

    Initial Commit

---
+++ InstaCut/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstaCut
+//
+//  Created by Kazim Gajiev on 15.02.2018.
+//  Copyright © 2018 Kazim Gajiev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
