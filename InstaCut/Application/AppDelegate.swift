edc7f567f7eb960b4f1ae429ee88fae43f3d5654
Kazim Gadjiev <mr.gadjiev2011@yandex.ru>
2020-10-12 10:23:45 +0300

    Clear old version

---
+++ InstaCut/Application/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  InstaCut
+//
+//  Created by Kazim Gajiev on 15.02.2018.
+//  Copyright © 2018 Kazim Gajiev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+                
+        return true
+    }
+
+}
+
---
+++ InstaCut/Application/Modules/App/AppCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  AppCoordinator.swift
+//  InstaCut
+//
+//  Created by Kazim Gadjiev on 12.10.2020.
+//  Copyright © 2020 Kazim Gajiev. All rights reserved.
+//
+
+import Foundation
